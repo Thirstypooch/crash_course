8805042503985a83a2f1a8dfcaa502b8f04e9004
Thirstypooch <thirstypooch@protonmail.com>
2022-04-26 12:18:51 -0500

    initializing scaffold

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:crash_course/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram',
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        home: Scaffold(body: Text('Building Insta!')));
+  }
+}
